17d87591ae94d0f1d42dda977b2c9c0e870ae5f1
Shahriar <shahriar3809@gmail.com>
2024-05-11 20:53:59 +0600

    JWT Implement

--- index.js
+++ index.js
@@ -2,12 +2,44 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const port = process.env.port || 5000;
 require("dotenv").config();
 
+
+const corsOption = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
 // Middleware
-app.use(cors());
+app.use(cors(corsOption));
 app.use(express.json());
+app.use(cookieParser());
+
+
+// Jwt middleware
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.send({ code: 401, message: "Un-authorized access" });
+  }
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+      if (error) {
+        return res.send({code: 401, message: "Un-authorized access" });
+      }
+
+      req.user = decoded;
+      
+
+      next();
+    });
+  }
+};
+
+
 
 // URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jryyhrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -24,155 +56,178 @@ async function run() {
   try {
     // await client.connect();
 
+    // JWT
+
+    // Generate Token when login
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+
+    // Clear token on logout
+    app.get("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
+
     const allBookCollection = client.db("dreamLibrary").collection("allBook");
     const borrowedBookCollection = client
       .db("dreamLibrary")
       .collection("borrowedBook");
 
-    const categoryCollection = client
-      .db("dreamLibrary")
-      .collection("category");
+    const categoryCollection = client.db("dreamLibrary").collection("category");
 
-    app.get("/allBook", async (req, res) => {
+
+    app.get("/allBook", verifyToken, async (req, res) => {
+      const tokenEmail = req.user.email;
+      if (!tokenEmail) {
+        return res.send({ code: 403, message: "Forbidden Access" });
+      }
+      
+
       const cursor = allBookCollection.find();
       const result = await cursor.toArray();
-      res.send(result)
-    });
-
-
-    app.get('/details/:id', async (req, res)=> {
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await allBookCollection.findOne(query)
       res.send(result);
-    })
+    });
 
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allBookCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.get("/borrowedBooks", async(req, res)=> {
+    app.get("/borrowedBooks", verifyToken, async (req, res) => {
+      const tokenEmail = req.user.email;
       const query = req.query;
+      if(tokenEmail !== query.email) {
+        return res.send({code: 403, message: "Forbidden Access"})
+      }
       const result = await borrowedBookCollection.find(query).toArray();
-        res.send(result);
+      res.send(result);
     });
 
-    app.get("/category", async(req, res)=> {
-        const query = req.body;
-        const result = await categoryCollection.find(query).toArray();
-        res.send(result)
-    })
-
-    app.get('/category/:name', async(req, res)=> {
-        const name = req.params.name;
-        const query = { category_name: name };
-        const result = await allBookCollection.find(query).toArray();
-        res.send(result)
-    })
-
- 
-
-    
-
-
-    app.post("/addBook", async(req, res)=> {
-        const newBook = req.body;
-        console.log(newBook)
-        const result = await allBookCollection.insertOne(newBook)
-        res.send(result)
+    app.get("/category", async (req, res) => {
+      const query = req.body;
+      const result = await categoryCollection.find(query).toArray();
+      res.send(result);
     });
 
-
-    app.post("/borrowed", async(req, res)=> {
-        const newBorrow = req.body;
-        // console.log(newBorrow)
-        const query = { email: newBorrow.email, bookId: newBorrow.bookId, isBorrowed: true };
-        const alreadyBorrowed = await borrowedBookCollection.findOne(query)
-        
-        if(alreadyBorrowed) {
-          return res.send('Already Borrowed This Book.')
-        }
-
-        const result = await borrowedBookCollection.insertOne(newBorrow);
-        
-
-        const updateDoc = { $inc: { quantity: -1 } };
-        const bookQuery = { _id: newBorrow._id };
-        const bookQuery1 = {_id: new ObjectId(newBorrow.bookId)}
-    
-        const updateQuantity = await borrowedBookCollection.updateOne(
-          bookQuery,
-          updateDoc
-        );
-        // console.log(updateQuantity)
-        const updateQuantity1 = await allBookCollection.updateOne(
-          bookQuery1,
-          updateDoc
-        );
-        
-        res.send(result)
+    app.get("/category/:name", async (req, res) => {
+      const name = req.params.name;
+      const query = { category_name: name };
+      const result = await allBookCollection.find(query).toArray();
+      res.send(result);
     });
 
+    app.post("/addBook", verifyToken, async (req, res) => {
+      const newBook = req.body;
+      const userEmail = newBook.email
+      // console.log(userEmail);
+      const tokenEmail = req.user.email;
+      if (tokenEmail !== userEmail) {
+        return res.send({ code: 403, message: "Forbidden Access" });
+      }
+      const result = await allBookCollection.insertOne(newBook);
+      res.send(result);
+    });
 
+    app.post("/borrowed", async (req, res) => {
+      const newBorrow = req.body;
+      // console.log(newBorrow)
+      const query = {
+        email: newBorrow.email,
+        bookId: newBorrow.bookId,
+        isBorrowed: true,
+      };
+      const alreadyBorrowed = await borrowedBookCollection.findOne(query);
 
+      if (alreadyBorrowed) {
+        return res.send("Already Borrowed This Book.");
+      }
 
+      const result = await borrowedBookCollection.insertOne(newBorrow);
 
+      const updateDoc = { $inc: { quantity: -1 } };
+      const bookQuery = { _id: newBorrow._id };
+      const bookQuery1 = { _id: new ObjectId(newBorrow.bookId) };
+
+      const updateQuantity = await borrowedBookCollection.updateOne(
+        bookQuery,
+        updateDoc
+      );
+      // console.log(updateQuantity)
+      const updateQuantity1 = await allBookCollection.updateOne(
+        bookQuery1,
+        updateDoc
+      );
+
+      res.send(result);
+    });
 
     app.delete("/returnBook/:id", async (req, res) => {
       const id = req.params.id;
       const query = { bookId: id };
       const result = await borrowedBookCollection.deleteOne(query);
-      console.log(id)
-      console.log(query)
+      console.log(id);
+      console.log(query);
       const bookQuery = { _id: new ObjectId(id) };
       // console.log(bookQuery)
-    const updateDoc = { $inc: { quantity: 1 } };
+      const updateDoc = { $inc: { quantity: 1 } };
       const updateQuantity1 = await allBookCollection.updateOne(
         bookQuery,
         updateDoc
       );
-      res.send({updateQuantity1, result});
+      res.send({ updateQuantity1, result });
     });
 
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollection.findOne(query);
+      res.send(result);
+    });
 
-
-
-
-
-        app.get("/details/:id", async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: new ObjectId(id) };
-          const result = await craftCollection.findOne(query);
-          res.send(result);
-        });
-
-        
-
-        app.put("/update/:id", async (req, res) => {
-          const id = req.params.id;
-          // console.log(id)
-          const filter = { _id: new ObjectId(id) };
-          const options = { upsert: true };
-          const newInfo = req.body;
-          // console.log(newInfo)
-          const updateBook = {
-            $set: {
-              bookName: newInfo.bookName,
-              photoUrl: newInfo.photoUrl,
-              rating: newInfo.rating,
-              author: newInfo.author,
-              category_name: newInfo.category_name,
-            },
-          };
-          const result = await allBookCollection.updateOne(
-            filter,
-            updateBook,
-            options
-          );
-          res.send(result);
-        });
-
-
-
-
-
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id)
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const newInfo = req.body;
+      // console.log(newInfo)
+      const updateBook = {
+        $set: {
+          bookName: newInfo.bookName,
+          photoUrl: newInfo.photoUrl,
+          rating: newInfo.rating,
+          author: newInfo.author,
+          category_name: newInfo.category_name,
+        },
+      };
+      const result = await allBookCollection.updateOne(
+        filter,
+        updateBook,
+        options
+      );
+      res.send(result);
+    });
 
     // Its optional
     // Send a ping to confirm a successful connection
@@ -193,6 +248,69 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is Running on port, ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // -------------------
+// // specific get api
+// app.get("/.../:?", verifyToken, async (req, res) => {
+//   // code
+//   const email = req.params.email;
+//   const tokenEmail = req.user.email;
+//   if (tokenEmail !== email) {
+//     res.status(403).send({ message: "Forbidden access" });
+//   }
+//   // res.send(data)
+//   // code
+// });
+
+// // -------------------
+
+// // Client---------------------------------
+
+// //Call in Login function
+// axios
+//   .post("...../jwt", { email: email }, { withCredentials: true })
+//   .then((data) => {
+//     console.log(data.data);
+//   });
+
+// // Call in logout function
+// axios("...../logout", { withCredentials: true }).then((data) => {
+//   console.log(data.data);
+// });
+
+// // specific data getting
+// axios("........./..?..", { withCredentials: true }).then((data) => {
+//   console.log(data.data);
+// });
+
+
+
+
+
+
+
+
+
+
+
 
 // const express = require('express');
 // const cors = require('cors')
